7ea23eeb5ef98de6aa0487904a5a3173f78212b2
Brijkishor24 <71825478+Brijkishor24@users.noreply.github.com>
2020-10-06 16:33:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 //https://teachablemachine.withgoogle.com/models/wSWeak6Pi/
+Webcam.set({
+    height:300,
+    width:350,
+    image_format:"png",
+    png_quality:90
+});
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+function take_snapshot() {
+    Webcam.snap(function(data_url){
+        document.getElementById("result").innerHTML='<img id="captured_image" src='+data_url+'>';
+    });
+}
+console.log("ml5 version",ml5.version);
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/wSWeak6Pi/model.json",modelloaded)
+function modelloaded(){
+    console.log("Model Loaded");
+
+}
